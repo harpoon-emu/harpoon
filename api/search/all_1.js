21f3e9b910f0abb4622c3193883af456f9037c5d
Michał Wysoczański <michalwy@gmail.com>
2015-05-17 18:59:51 +0000

    Auto-update API documentation

--- api/search/all_1.js
+++ api/search/all_1.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['add_5fcomponent',['add_component',['../df/d54/classharpoon_1_1hardware__component.html#a2563d6b4260a653934b846bc6a3fd7c3',1,'harpoon::hardware_component']]],
+  ['add_5fcomponent',['add_component',['../df/d54/classharpoon_1_1hardware__component.html#a79f39271e79427df69e4114bac1dc0ef',1,'harpoon::hardware_component']]],
   ['atomic_5fcycle',['atomic_cycle',['../d3/df0/namespaceharpoon_1_1execution.html#af46fa393fcbd4ef3d69837509b47b0aa',1,'harpoon::execution']]],
   ['atomic_5ftick',['atomic_tick',['../d9/d7f/namespaceharpoon_1_1clock_1_1generator.html#a48edb1b97e51e143b3a55f7b39e89b08',1,'harpoon::clock::generator::atomic_tick()'],['../df/dba/namespaceharpoon_1_1clock.html#aa515855edc74d46182266e19002c1708',1,'harpoon::clock::atomic_tick()']]]
 ];
--- api/search/functions_0.js
+++ api/search/functions_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['add_5fcomponent',['add_component',['../df/d54/classharpoon_1_1hardware__component.html#a2563d6b4260a653934b846bc6a3fd7c3',1,'harpoon::hardware_component']]]
+  ['add_5fcomponent',['add_component',['../df/d54/classharpoon_1_1hardware__component.html#a79f39271e79427df69e4114bac1dc0ef',1,'harpoon::hardware_component']]]
 ];
