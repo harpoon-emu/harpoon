eaa39349fa8b9b18bbbfa065a9991d1909e2dd4c
Michał Wysoczański <michalwy@gmail.com>
2015-05-18 20:21:01 +0000

    Auto-update API documentation

--- api/search/functions_2.js
+++ api/search/functions_2.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['cleanup',['cleanup',['../df/d54/classharpoon_1_1hardware__component.html#a20f69cd3610ba192bfbaf18a81945308',1,'harpoon::hardware_component']]],
+  ['cleanup',['cleanup',['../df/d54/classharpoon_1_1hardware__component.html#a20f69cd3610ba192bfbaf18a81945308',1,'harpoon::hardware_component::cleanup()'],['../da/df3/classharpoon_1_1memory_1_1linear__memory.html#a32c42059e55bd01b38333bdf20ae4453',1,'harpoon::memory::linear_memory::cleanup()']]],
   ['clock',['clock',['../d5/d84/classharpoon_1_1clock_1_1clock.html#a3664d6a551907179aa02dcca140f87f7',1,'harpoon::clock::clock']]]
 ];
