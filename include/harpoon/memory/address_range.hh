e5820d0385c81c6efbe038bee55164b2ed73c3e8
Michał Wysoczański <michalwy@gmail.com>
2020-08-02 18:25:49 +0200

    address_range: Mark most of the methods as constexpr

--- include/harpoon/memory/address_range.hh
+++ include/harpoon/memory/address_range.hh
@@ -18,89 +18,89 @@ namespace memory {
  */
 class address_range {
 public:
-	address_range() {
+	constexpr address_range() {
 		set_range(0, 0);
 	}
 
-	address_range(const address &start, const address &end) {
+	constexpr address_range(const address &start, const address &end) {
 		set_range(start, end);
 	}
 
 	address_range(const address_range &) = default;
 	address_range &operator=(const address_range &) = default;
 
-	bool operator==(const address_range &second) const {
+	constexpr bool operator==(const address_range &second) const {
 		return get_start() == second.get_start() && get_end() == second.get_end();
 	}
 
-	bool operator!=(const address_range &second) const {
+	constexpr bool operator!=(const address_range &second) const {
 		return get_start() != second.get_start() || get_end() != second.get_end();
 	}
 
-	operator bool() const {
+	constexpr operator bool() const {
 		return !is_empty();
 	}
 
-	bool is_empty() const {
+	constexpr bool is_empty() const {
 		return _start == _end;
 	}
 
-	address get_start() const {
+	constexpr address get_start() const {
 		return _start;
 	}
 
-	void set_start(address start) {
+	constexpr void set_start(address start) {
 		_start = start;
 		_normalize();
 	}
 
-	address get_end() const {
+	constexpr address get_end() const {
 		return _end - 1;
 	}
 
-	void set_end(address end) {
+	constexpr void set_end(address end) {
 		_end = end + 1;
 		_normalize();
 	}
 
-	std::uint_fast64_t get_length() const {
+	constexpr std::uint_fast64_t get_length() const {
 		return _end - _start;
 	}
 
-	void set_length(std::uint_fast64_t length) {
+	constexpr void set_length(std::uint_fast64_t length) {
 		_end = _start + length;
 	}
 
-	void set_range(address start, address end) {
+	constexpr void set_range(address start, address end) {
 		_start = start;
 		_end = end + 1;
 		_normalize();
 	}
 
-	void set_start_and_length(address start, std::uint_fast64_t length) {
+	constexpr void set_start_and_length(address start, std::uint_fast64_t length) {
 		_start = start;
 		set_length(length);
 	}
 
-	bool has_address(address address) const {
+	constexpr bool has_address(address address) const {
 		return address >= get_start() && address <= get_end();
 	}
 
-	address get_offset(address address) const {
+	constexpr address get_offset(address address) const {
 		return address - get_start();
 	}
 
-	bool overlaps(const address_range &second) const {
+	constexpr bool overlaps(const address_range &second) const {
 		return has_address(second.get_start()) || has_address(second.get_end());
 	}
 
-	address_range get_intersection(const address_range &r) const {
+	constexpr address_range get_intersection(const address_range &r) const {
 		address_range n = r;
 		n.intersect(*this);
 		return n;
 	}
 
-	void intersect(const address_range &r) {
+	constexpr void intersect(const address_range &r) {
 		address s = std::max(_start, r._start);
 		address e = std::min(_end, r._end);
 		if (e < s) {
@@ -110,7 +110,7 @@ public:
 		_end = e;
 	}
 
-	static inline address max() {
+	static constexpr address max() {
 		return std::numeric_limits<address>::max();
 	}
 
@@ -131,7 +131,7 @@ public:
 	}
 
 private:
-	void _normalize() {
+	constexpr void _normalize() {
 		if (_start > _end - 1) {
 			address t = _start;
 			_start = _end - 1;
